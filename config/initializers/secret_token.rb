5a93c3c68758fea2587afda9fc71e9efe0adca4f
Matt Schmaus <mschmaus_greenhouse@Matts-GH-MacBook-Air.local>
2015-08-09 00:36:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CleanEats::Application.config.secret_token = '2906335cfefc4b82bbd296840e1238ed2829bc12b4c9b650f4996ade6c67e39729351fd384b380b5800107193cdead78bc43b1c6d353be7a1a47c9fdbb477586'
